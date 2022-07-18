2e9242640fb4b024262d09b997274ede974bcb8e
satoyu19 <96398365+satoyu19@users.noreply.github.com>
2022-07-18 11:36:49 +0900

    Initial Commit

---
+++ CombineSample19/CombineSample19App.swift
@@ -0,0 +1,17 @@
+//
+//  CombineSample19App.swift
+//  CombineSample19
+//
+//  Created by cmStudent on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct CombineSample19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
